32642cf6c6bc4d47d183d36bab76906cbd03b534
Vladix97 <gramovv@gmail.com>
2017-07-02 15:57:20 +0300

    finished advanced functuions

--- JavaScriptAdvanced/04.AdvancedFunctions/AdvancedFunctionsHomework/p06_breakfastRobot.js
+++ JavaScriptAdvanced/04.AdvancedFunctions/AdvancedFunctionsHomework/p06_breakfastRobot.js
@@ -1,7 +1,71 @@
 function solve() {
-    
 
-    return function () {
-        
+    let elements = new Map();
+    elements.set('protein', 0);
+    elements.set('carbohydrate', 0);
+    elements.set('fat', 0);
+    elements.set('flavour', 0);
+
+    let products = new Map();
+    products.set('apple', { carbohydrate: 1, flavour: 2 });
+    products.set('coke', { carbohydrate: 10, flavour: 20 });
+    products.set('burger', { carbohydrate: 5, fat: 7, flavour: 3 });
+    products.set('omelet', { protein: 5, fat: 1, flavour: 1 });
+    products.set('cheverme', { protein: 10, carbohydrate: 10, fat: 10, flavour: 10 });
+
+    return function (input) {
+        let tokens = input.split(' ');
+
+        let commandType = tokens[0];
+        if (commandType === 'restock') {
+            let product = tokens[1];
+            let quantity = Number(tokens[2]);
+
+            elements.set(product, elements.get(product) + quantity);
+            return ('Success');
+        } else if (commandType === 'prepare') {
+            let product = tokens[1];
+            let quantity = Number(tokens[2]);
+
+            let productAsProduct = products.get(product);
+            let neededElements = Object.keys(productAsProduct);
+            for (let neededElement of neededElements) {
+                let neededQuantity = productAsProduct[neededElement] * quantity;
+                if (elements.get(neededElement) < neededQuantity) {
+                    return (`Error: not enough ${neededElement} in stock`);
+                    break;
+                }
+            }
+
+            for (let neededElement of neededElements) {
+                let neededQuantity = productAsProduct[neededElement] * quantity;
+                let current = elements.get(neededElement);
+                elements.set(neededElement, current - neededQuantity);
+            }
+
+            return ('Success');
+        } else if (commandType === 'report') {
+            return (`protein=${elements.get('protein')} carbohydrate=${elements.get('carbohydrate')} fat=${elements.get(`fat`)} flavour=${elements.get('flavour')}`);
+        }
     }
 }
+
+let manager = solve();
+
+
+/*
+ restock flavour 10
+ prepare apple 1
+ restock fat 10
+ prepare burger 1
+ report
+ */
+
+/*
+manager("restock carbohydrate 10"); // Success
+manager("restock flavour 10");
+manager("prepare apple 1");
+manager("restock fat 10");
+manager("prepare burger 1");
+manager("report");
+*/
--- JavaScriptAdvanced/04.AdvancedFunctions/AdvancedFunctionsHomework/p07_monkeyPatcher.js
+++ JavaScriptAdvanced/04.AdvancedFunctions/AdvancedFunctionsHomework/p07_monkeyPatcher.js
@@ -1,3 +1,32 @@
-function solve() {
+function solution(input) {
+    switch (input) {
+        case 'upvote':
+            this.upvotes++;
+            break;
+        case 'downvote':
+            this.downvotes++;
+            break;
+        case 'score':
+            let currUpvotes = this.upvotes;
+            let currDownvotes = this.downvotes;
+            let rating = 'new';
+            if (currUpvotes + currDownvotes >= 10) {
+                if (currUpvotes > 0.66 * (currUpvotes + currDownvotes)) {
+                    rating = 'hot'
+                } else if (currDownvotes > currUpvotes) {
+                    rating = 'unpopular';
+                } else if (currUpvotes > 100 || currDownvotes > 100) {
+                    rating = 'controversial';
+                }
 
+                if (currUpvotes + currDownvotes > 50) {
+                    let modifier = Math.ceil(0.25 * Math.max(currUpvotes, currDownvotes));
+                    currUpvotes += modifier;
+                    currDownvotes += modifier;
+                }
+            }
+
+            let score = currUpvotes - currDownvotes;
+            return [currUpvotes, currDownvotes, score, rating];
+    }
 }
