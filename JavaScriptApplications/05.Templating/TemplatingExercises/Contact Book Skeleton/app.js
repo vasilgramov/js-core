011aae1bfdcb7c2ef2be032afc3c9f6afbee16a2
Vladix97 <gramovv@gmail.com>
2017-08-06 14:55:30 +0300

    contact book

--- JavaScriptApplications/05.Templating/TemplatingExercises/Contact Book Skeleton/app.js
+++ JavaScriptApplications/05.Templating/TemplatingExercises/Contact Book Skeleton/app.js
@@ -4,20 +4,59 @@ $(() => {
         contacts : []
     };
 
-    const template = {
-        list: []
-    };
+    const templates = {};
 
+    const listTarget = $('#list').find('.content');
+    const detailsTarget = $('#details').find('.content');
+
+    loadData();
     loadTemplates();
-    loadContacts();
+
+    function loadData() {
+        $.get('data.json').then(getData);
+
+        function getData(data) {
+            context.contacts = (data).map(c => {
+                c.active = false;
+                return c;
+            });
+        }
+    }
 
     function loadTemplates() {
+        Promise.all([
+            $.get('templates/contact.html'),
+            $.get('templates/contactsList.html'),
+            $.get('templates/details.html')
+        ]).then(getTemplates);
+
+        function getTemplates([contact, contactsList, details]) {
+            Handlebars.registerPartial('contact', contact);
+
+            templates['contactList'] = Handlebars.compile(contactsList);
+            templates['details'] = Handlebars.compile(details);
+
+            renderContacts();
+        }
     }
 
-    function loadContacts() {
-        console.log(template.list);
-        let list = Handlebars.compile(template.list);
-        $('#list').find('.content').html(list(context.contacts));
+    function renderContacts() {
+        listTarget.html(templates.contactList(context));
+        attachClickEvent();
     }
 
+    function attachClickEvent() {
+        $('.contact').click((e) => {
+            let index = $(e.target).closest('.contact').attr('data-id');
+            renderDetails(index);
+        });
+    }
+
+    function renderDetails(index) {
+        context.contacts.forEach(c => c.active = false);
+        context.contacts[index].active = true;
+
+        renderContacts();
+        detailsTarget.html(templates.details(context.contacts[index]));
+    }
 });
