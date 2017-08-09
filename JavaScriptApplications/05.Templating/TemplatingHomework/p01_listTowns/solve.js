af0798056eec4b786e7ab19bd83bc0f51ea9e2a4
Vladix97 <gramovv@gmail.com>
2017-08-09 17:41:44 +0300

    template homework

---
+++ JavaScriptApplications/05.Templating/TemplatingHomework/p01_listTowns/solve.js
@@ -0,0 +1,58 @@
+$(() => {
+
+    const townsInput = $('#towns');
+    const btnLoad = $('#btnLoadTowns');
+    const targetList = $('#townsList');
+
+    const context = {
+        towns: []
+    };
+    const templates = {};
+
+    btnLoad.click(loadTowns);
+
+    async function loadTowns() {
+
+        getContext();
+
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        loadTemplates().then((data) => {
+            registerTemplates(data);
+            renderTowns();
+        });
+
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        registerTemplates(await loadTemplates());
+        renderTowns();
+
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+
+    function getContext() {
+
+        context.towns = townsInput.val().split(',')
+            .filter(town => town !== '')
+            .map((townName) => ({ townName: townName.trim() }));
+        townsInput.val('');
+    }
+
+    function loadTemplates() {
+        return Promise.all([$.get('town.hbs'), $.get('townList.hbs')]);
+    }
+
+    function registerTemplates([town, townList]) {
+        console.log("REGISTERING");
+
+        Handlebars.registerPartial('town', town);
+
+        templates.townList = Handlebars.compile(townList);
+    }
+
+    function renderTowns() {
+        console.log("RENDERING");
+
+        console.log(context.towns);
+        targetList.html(templates.townList(context));
+    }
+});
---
+++ JavaScriptApplications/05.Templating/TemplatingHomework/p02_httpStatusCats/template.js
@@ -0,0 +1,53 @@
+$(() => {
+
+    const SHOW_STATUS_CODE = 'Show status code';
+    const HIDE_STATUS_CODE = 'Hide status code';
+
+    const catsListDiv = $('#allCats');
+    const context = {
+        cats: []
+    };
+    const templates = { };
+
+
+
+    loadData();
+
+    loadTempates();
+
+    function loadData() {
+        context.cats = window.cats;
+    }
+
+    async function loadTempates() {
+
+        let [catTemplate, catListTemplate] = await
+            Promise.all([
+                $.get('templates/cat.hbs'),
+                $.get('templates/catList.hbs')
+            ]);
+
+        Handlebars.registerPartial('cat', catTemplate);
+        templates.catList = Handlebars.compile(catListTemplate);
+
+        renderCatTemplate();
+    }
+
+    function renderCatTemplate() {
+
+        catsListDiv.html(templates.catList(context));
+
+        $('.btn-primary').click(showStatusCode);
+    }
+
+    function showStatusCode() {
+        let button = $(this);
+        if (button.text() === SHOW_STATUS_CODE) {
+            button.next().css('display', 'block');
+            button.text(HIDE_STATUS_CODE);
+        } else {
+            button.next().css('display', 'none');
+            button.text(SHOW_STATUS_CODE);
+        }
+    }
+});
